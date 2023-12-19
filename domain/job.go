4699bae742702a1dd098bedf2d5260f6fd601719
Marcelo T. dos Santos <marcelo.teixeira@momox.biz>
2023-12-19 15:23:08 -0300

    Simplify job entity for readability

--- domain/job.go
+++ domain/job.go
@@ -8,26 +8,26 @@ import (
 )
 
 type Job struct {
-	ID string `json:"job_id" valid:"uuid" gorm:"type:uuid;primary_key"`
-	// path to the folder container all converted files
-	OutputBucketPath string `json:"output_bucket_path" valid:"notnull"`
-	Status           string `json:"status" valid:"notnull"`
-	// a pointer to the entity Video
-	Video     *Video    `json:"video" valid:"-"`
-	VideoID   string    `json:"-" valid:"-" gorm:"column:video_id;type:uuid;notnull"`
-	Error     string    `json:"error" valid:"-"`
-	CreatedAt time.Time `json:"created_at" valid:"-"`
-	UpdatedAt time.Time `json:"updated_at" valid:"-"`
+	ID               string    `json:"job_id" valid:"uuid" gorm:"type:uuid;primary_key"`
+	OutputBucketPath string    `json:"output_bucket_path" valid:"notnull"`
+	Status           string    `json:"status" valid:"notnull"`
+	Video            *Video    `json:"video" valid:"-"`
+	VideoID          string    `json:"-" valid:"-" gorm:"column:video_id;type:uuid;notnull"`
+	Error            string    `json:"error" valid:"-"`
+	CreatedAt        time.Time `json:"created_at" valid:"-"`
+	UpdatedAt        time.Time `json:"updated_at" valid:"-"`
 }
 
 func NewJob(output string, status string, video *Video) (*Job, error) {
 	job := Job{
+		ID:        uuid.NewV4().String(),
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+
 		OutputBucketPath: output,
 		Status:           status,
 		Video:            video,
 	}
-
-	job.prepare()
 
 	err := job.Validate()
 
@@ -36,12 +36,6 @@ func NewJob(output string, status string, video *Video) (*Job, error) {
 	}
 
 	return &job, nil
-}
-
-func (job *Job) prepare() {
-	job.ID = uuid.NewV4().String()
-	job.CreatedAt = time.Now()
-	job.UpdatedAt = time.Now()
 }
 
 func (job *Job) Validate() error {
